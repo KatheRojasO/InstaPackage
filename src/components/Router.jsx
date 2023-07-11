4885dfac5b7631cc19286f3f2ae790aaa94cfd8a
KatheRojasO <kathe.rojaso@gmail.com>
2023-07-11 22:47:23 +0200

    refactor name of files

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "../pages/HomePage";
-import TrackingSection from "../pages/TrackingSection";
-import ParcelSection from "../pages/ParcelSection";
+import Home from "../pages/Home";
+import Parcels from "../pages/Parcels";
+import ParcelDetail from "../pages/ParcelDetail";
 import NotFound from "./NotFound";
 
 export default function Router({ orders }) {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/parcels" element={<TrackingSection orders={orders} />} />
-        <Route
-          path="/parcels/:parcel_id"
-          element={<ParcelSection orders={orders} />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/parcels" element={<Parcels orders={orders} />} />
+        <Route path="/parcels/:id" element={<ParcelDetail orders={orders} />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
