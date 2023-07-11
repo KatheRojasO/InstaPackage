0a31bc3d10b2ed0527f4b96de8cc95345fa807bf
KatheRojasO <kathe.rojaso@gmail.com>
2023-07-11 10:59:29 +0200

    Implement backup function with localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import Router from "./components/Router";
 import Loading from "./components/Loading";
 import Error from "./components/Error";
 import "./style/style.sass";
 
 export default function App() {
-   const [status, setStatus] = useState(0);
-   const [orders, setOrders] = useState([]);
+  const [status, setStatus] = useState(0);
+  const [orders, setOrders] = useState([]);
 
-   async function fetchData () {
-    try {
-      const response = await fetch("https://my.api.mockaroo.com/insta-orders.json?key=e49e6840");
-      const ordersData = await response.json();
-      setStatus(1)
-      setOrders(ordersData)
-    }
-    catch (error) {
-      setStatus(2)
-    }
-   }
+  async function fetchData() {
+    return axios
+      .get("https://my.api.mockaroo.com/insta-orders.json?key=e49e6840")
+      .then((response) => {
+        localStorage.setItem("userData", JSON.stringify(response.data));
+        setStatus(1);
+        setOrders(response.data);
+      })
+      .catch(() => {
+        let cachedData = localStorage.getItem("userData");
+        setOrders(JSON.parse(cachedData));
+        if (cachedData != null) {
+          setStatus(1);
+          setOrders(JSON.parse(cachedData));
+        } else {
+          setStatus(2);
+        }
+      });
+  }
 
-   useEffect(() => {
+  useEffect(() => {
     fetchData();
-   }, []);
+  }, []);
 
   return (
     <div className="App">
-      {status === 0 && <Loading/>}
-      {status === 1 && <Router orders={orders}/>}
-      {status === 2 && <Error/>}   
+      {status === 0 && <Loading />}
+      {status === 1 && <Router orders={orders} />}
+      {status === 2 && <Error />}
     </div>
   );
 }
-
